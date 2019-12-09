2f00f493f2f66a738d30d553947a7edf307f0666
Christoffer Sundqvist <christoffer.sundqvist@consid.se>
2019-12-09 15:55:55 +0100

    JWT returning login, email validation registration

--- Synk/Controllers/v1/IdentityController.cs
+++ Synk/Controllers/v1/IdentityController.cs
@@ -21,8 +21,32 @@ namespace Synk.Controllers.v1
         [HttpPost(ApiRoutes.Identity.Register)]
         public async Task<IActionResult> Register([FromBody] UserRegistrationRequest request)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage))
+                });
+            }
             var authResponse = await _identityService.RegisterAsync(request.Email, request.Password);
             if(!authResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResponse.ErrorMessages
+                });
+            };
+            return Ok(new AuthSuccessResponse
+            {
+                Token = authResponse.Token
+            });
+        }
+
+        [HttpPost(ApiRoutes.Identity.Login)]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest request)
+        {
+            var authResponse = await _identityService.LoginAsync(request.Email, request.Password);
+            if (!authResponse.Success)
             {
                 return BadRequest(new AuthFailedResponse
                 {
--- Synk/Services/IIdentityService.cs
+++ Synk/Services/IIdentityService.cs
@@ -9,5 +9,7 @@ namespace Synk.Services
     public interface IIdentityService
     {
         Task<AuthenticationResult> RegisterAsync(string email, string password);
+
+        Task<AuthenticationResult> LoginAsync(string email, string password);
     }
 }
