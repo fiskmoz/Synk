6491d094109f9cb87bda599eeccd08eeb6edf5cf
fiskmoz <fiskmoz96@gmail.com>
2019-12-09 15:33:57 +0100

    added JWT token authentication for post controller

--- Synk/Contracts/v1/ApiRoutes.cs
+++ Synk/Contracts/v1/ApiRoutes.cs
@@ -18,5 +18,12 @@ namespace Synk.Contracts
             public const string Update = Base + "/posts/{postId}";
             public const string Delete = Base + "/posts/{postId}";
         }
+
+        public static class Identity
+        {
+            public const string Login = Base + "/identity/login";
+
+            public const string Register = Base + "/identity/register";
+        }
     }
 }
--- Synk/Startup.cs
+++ Synk/Startup.cs
@@ -54,6 +54,8 @@ namespace Synk
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseAuthentication();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
